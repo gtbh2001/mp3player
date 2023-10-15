0fe2cd7e27233aa940df03f31b57971cc2f33874
gtbh2001 <gtbh2001@gmail.com>
2023-10-15 07:10:51 -0300

    alterado velocidade do monitor serial

--- src/main.cpp
+++ src/main.cpp
@@ -44,12 +44,12 @@ void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
 #if (defined ESP32)
-  FPSerial.begin(9600, SERIAL_8N1, /*rx =*/D3, /*tx =*/D2);
+  FPSerial.begin(19200, SERIAL_8N1, /*rx =*/D3, /*tx =*/D2);
 #else
-  FPSerial.begin(9600);
+  FPSerial.begin(19200);
 #endif
 
-  Serial.begin(9600);
+  Serial.begin(19200);
 
   Serial.println();
   Serial.println(F("DFRobot DFPlayer Mini Demo"));
